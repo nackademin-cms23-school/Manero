e9dd23f041339217194ffa86482d7425828ae2b0
HugoEddesten <hugo.eddesten@yh.nackademin.se>
2024-05-13 15:10:06 +0200

    commitEdit Account info

---
+++ Frontend/Controllers/ProfileController.cs
@@ -0,0 +1,36 @@
+﻿using Frontend.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace Frontend.Controllers;
+
+public class ProfileController(HttpClient http, IConfiguration config) : Controller
+{
+    private readonly HttpClient _http = http;
+    private readonly IConfiguration _config = config;
+
+    public IActionResult Index()
+    {
+        return View("Profile");
+    }
+
+    public async Task<IActionResult> Edit()
+    {
+        var result = await _http.GetAsync("url");
+        var responseContent = await result.Content.ReadAsStringAsync();
+        EditAccountViewModel? model = JsonConvert.DeserializeObject<EditAccountViewModel>(responseContent);
+        return View(model);
+    }
+
+    public async Task<IActionResult> EditAccount(EditAccountViewModel model)
+    {
+        model.UserId = "9f8b99e0-9bc8-4c51-881f-07542689e9ca";
+
+
+        var response = await _http.PutAsJsonAsync("url", model);
+
+        return RedirectToAction("Edit");
+
+    }
+}
---
+++ Frontend/ViewModels/EditAccountViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Frontend.ViewModels;
+
+public class EditAccountViewModel
+{
+    public string UserId { get; set; } = null!;
+
+    [Required(ErrorMessage = "A first name is required")]
+    public string FirstName { get; set; } = null!;
+
+    [Required(ErrorMessage = "A last name is required")]
+    public string LastName { get; set; } = null!;
+
+    [Required(ErrorMessage = "An email address is required")]
+    [RegularExpression(@"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", ErrorMessage = "Email not valid")]
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; } = null!;
+
+    public string? PhoneNumber { get; set; }
+}
