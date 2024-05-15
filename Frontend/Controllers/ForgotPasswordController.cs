aa95cc9e9cc0551a67ec87fdf8d54c31583a25c2
Annelieno1 <gydano1@hotmail.com>
2024-05-15 15:09:03 +0200

    Styling to forgotpassword added.

--- Frontend/Controllers/ForgotPasswordController.cs
+++ Frontend/Controllers/ForgotPasswordController.cs
@@ -1,10 +1,13 @@
 ﻿using Frontend.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace Frontend.Controllers;
 
-public class ForgotPasswordController : Controller
+public class ForgotPasswordController(HttpClient http) : Controller
 {
+    private readonly HttpClient _http = http;
+
     [HttpGet]
     [Route("/forgotpassword")]
     public IActionResult ForgotPassword()
@@ -18,7 +21,27 @@ public class ForgotPasswordController : Controller
     {
         if(ModelState.IsValid)
         {
+            var result = await _http.PostAsJsonAsync("", model);
+            if(result.IsSuccessStatusCode)
+            {
+                return RedirectToAction("ForgotPasswordNewPassword", "ForgotPassword");
+                //Ska Skicka en kod till email. 
+                //Skriva in kod 
+                //Få skriva in ett nytt lösenord
+            }
+            else if(result.StatusCode == HttpStatusCode.Conflict)
+            {
+                ViewData["StatusMessage"] = "The emailaddress does not exists.";
+            }
+            else
+            {
+                ViewData["StatusMessage"] = "Something went wrong. Please try again.";
+            }
 
+        }
+        else
+        {
+            ViewData["StatusMessage"] = "Please enter all information correctly.";
         }
         return View(model);
     }
