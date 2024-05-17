e359e1c0720bfb45452964fea2f9f1cacefe824f
Annelieno1 <gydano1@hotmail.com>
2024-05-17 15:49:37 +0200

    ForgotPassword  done

---
+++ Frontend/Controllers/AccountCreatedController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Frontend.Controllers;
+
+public class AccountCreatedController : Controller
+{
+    public IActionResult AccountCreated()
+    {
+        return View();
+    }
+}
