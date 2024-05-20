8c6cacee01268da2a4d76d246fce8ccf0180c990
EmperorBlizzard <emil111@live.se>
2024-05-20 17:23:07 +0200

    added account verification js

--- Frontend/wwwroot/js/site.js
+++ Frontend/wwwroot/js/site.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-    codeVerification()
+    forgotPasswordVerification()
+    accountVerification()
 })
 
-function codeVerification() {
+function forgotPasswordVerification() {
     const form = document.querySelector(".forgotpassword-verification-form")
     const inputs = form.querySelectorAll("input")
 
@@ -14,6 +15,22 @@ function codeVerification() {
 
                 e.target.value = result
                 console.log(value)
+            }
+        })
+    })
+}
+
+function accountVerification(){
+    const form = document.querySelector(".verification-form")
+    const inputs = form.querySelectorAll("input")
+
+    inputs.forEach(input => {
+        input.addEventListener("keyup", (e) => {
+            if(e.target.value.length > 1) {
+                let value = e.target.value
+                let result = value.slice(1)
+
+                e.target.value = result
             }
         })
     })
