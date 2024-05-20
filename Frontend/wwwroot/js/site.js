ceaeed28184aaaacc71dedb645b6f4125a08c8e7
Raouf <apokaa123@live.se>
2024-05-20 22:26:10 +0200

    Onboarding almost done!

--- Frontend/wwwroot/js/site.js
+++ Frontend/wwwroot/js/site.js
@@ -1,41 +1,41 @@
 document.addEventListener("DOMContentLoaded", function () {
-    //forgotPasswordVerification()
-    //accountVerification()
+    forgotPasswordVerification()
+    accountVerification()
     onboardingFunction()
 })
 
-//function forgotPasswordVerification() {
-//    const form = document.querySelector(".forgotpassword-verification-form")
-//    const inputs = form.querySelectorAll("input")
+function forgotPasswordVerification() {
+    const form = document.querySelector(".forgotpassword-verification-form")
+    const inputs = form.querySelectorAll("input")
 
-//    inputs.forEach(input => {
-//        input.addEventListener("keyup", (e) => {
-//            if (e.target.value.length > 1) {
-//                let value = e.target.value
-//                let result = value.slice(1)
+    inputs.forEach(input => {
+        input.addEventListener("keyup", (e) => {
+            if (e.target.value.length > 1) {
+                let value = e.target.value
+                let result = value.slice(1)
 
-//                e.target.value = result
-//                console.log(value)
-//            }
-//        })
-//    })
-//}
+                e.target.value = result
+                console.log(value)
+            }
+        })
+    })
+}
 
-//function accountVerification(){
-//    const form = document.querySelector(".verification-form")
-//    const inputs = form.querySelectorAll("input")
+function accountVerification(){
+    const form = document.querySelector(".verification-form")
+    const inputs = form.querySelectorAll("input")
 
-//    inputs.forEach(input => {
-//        input.addEventListener("keyup", (e) => {
-//            if(e.target.value.length > 1) {
-//                let value = e.target.value
-//                let result = value.slice(1)
+    inputs.forEach(input => {
+        input.addEventListener("keyup", (e) => {
+            if(e.target.value.length > 1) {
+                let value = e.target.value
+                let result = value.slice(1)
 
-//                e.target.value = result
-//            }
-//        })
-//    })
-//}
+                e.target.value = result
+            }
+        })
+    })
+}
 
 function onboardingFunction() {
     console.log("Hej")
