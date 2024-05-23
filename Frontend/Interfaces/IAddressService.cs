46c484a63f16620b71498606cf204e712df5cfac
HugoEddesten <hugo.eddesten@yh.nackademin.se>
2024-05-23 23:13:28 +0200

    Full Address and Account functionality

--- Frontend/Interfaces/IAddressService.cs
+++ Frontend/Interfaces/IAddressService.cs
@@ -1,13 +1,14 @@
 ﻿using Frontend.ViewModels.Address;
+using System.Security.Claims;
 
 namespace Frontend.Interfaces
 {
     public interface IAddressService
     { 
         Task<AddressViewModel> CreateAsync(AddressForm form);
-        Task<bool> DeleteAsync(string userId, string id);
-        Task<IEnumerable<Address>> GetAllAsync(string userId);
-        Task<Address> GetOneAsync(string userId, string addressId);
+        Task<bool> DeleteAsync(ClaimsPrincipal user, string addressId);
+        Task<IEnumerable<Address>> GetAllAsync(ClaimsPrincipal user);
+        Task<Address> GetOneAsync(ClaimsPrincipal user, string addressId);
         Task<Address> UpdateAsync(Address form);
     }
 }
